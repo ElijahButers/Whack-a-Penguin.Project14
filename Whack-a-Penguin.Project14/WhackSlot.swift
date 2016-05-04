98afe48226873a9185d675ce7f76501b7926c047
ElijahButers <Elijahbuters@gmail.com>
2016-05-04 20:52:03 +0300

    Added a WhackSlot  subclass of SKNode. Put import SpriteKit inside of it.

---
+++ Whack-a-Penguin.Project14/WhackSlot.swift
@@ -0,0 +1,14 @@
+//
+//  WhackSlot.swift
+//  Whack-a-Penguin.Project14
+//
+//  Created by User on 5/4/16.
+//  Copyright © 2016 Elijah Buters. All rights reserved.
+//
+
+import SpriteKit
+import UIKit
+
+class WhackSlot: SKNode {
+
+}

1bbb971a8f22258a8483b6f97c822eafe3a978cd
ElijahButers <Elijahbuters@gmail.com>
2016-05-06 14:34:00 +0300

    Added code to configureAtPosition method that adds a penguin directly beneath a hole.

--- Whack-a-Penguin.Project14/WhackSlot.swift
+++ Whack-a-Penguin.Project14/WhackSlot.swift
@@ -18,6 +18,17 @@ class WhackSlot: SKNode {
         
         let sprite = SKSpriteNode(imageNamed: "whackHole")
         addChild(sprite)
+        
+        let cropNode  = SKCropNode()
+        cropNode.position = CGPoint(x: 0, y: 15)
+        cropNode.zPosition = 1
+        cropNode.maskNode = nil
+        
+        charNode = SKSpriteNode(imageNamed: "penguinGood")
+        charNode.position = CGPoint(x: 0, y: -90)
+        charNode.name = "character"
+        cropNode.addChild(charNode)
+        addChild(cropNode)
     }
 
 }
